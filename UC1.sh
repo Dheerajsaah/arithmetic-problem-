cc7fc3a1d90f1a98078cb16eabc39fa01c4cbd44
dherajsaah <dheerajsaah248@gmail.com>
2022-08-05 16:09:17 +0530

    Ask user for three input-a,b & c.

---
+++ UC1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/ -x
+echo -n " Enter the first number : "
+read n1
+echo -n " Enter the second number : "
+read n2
+echo -n " Enter the third number : "
+read n3
+
+echo "You entered $n1 $n2 $n3"
+echo ""
