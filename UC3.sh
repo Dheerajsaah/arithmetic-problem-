4a3792e7b1037372a4371c73bb285e55c365132a
dherajsaah <dheerajsaah248@gmail.com>
2022-08-05 16:33:40 +0530

    Compute c+a / b.

---
+++ UC3.sh
@@ -0,0 +1,27 @@
+echo -n " Enter the first number : "
+read n1
+echo -n " Enter the second number : "
+read n2
+echo -n " Enter the third number : "
+read n3
+
+echo "You entered $n1 $n2 $n3"
+echo ""
+
+#Compute a + b * c
+operation1=$((n1+$((n2*$n3))))
+
+echo "Computation 1 :     a + b * c = $operation1"
+echo ""
+
+#Compute a * b + c  
+operation2=$(($((n1*$n2))+$n3))
+
+echo "Computation 2 :     a * b + c = $operation2"
+echo ""
+
+#Compute c + a / b
+operation3=$((n3+$((n1/$n2))))
+
+echo "Computation 3 :     c + a / b = $operation3"
+echo ""
